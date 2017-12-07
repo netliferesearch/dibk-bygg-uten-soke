76eeebfc01de9a6d730260f90be615a1f2832f54
Anders Olsen Sandvik <Andersos@users.noreply.github.com>
2017-12-07 17:02:45 +0100

    Add all the basis questions

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      intro: true,
+      intro: false, // CHANGE TO TRUE
     };
     autobind(this);
   }
